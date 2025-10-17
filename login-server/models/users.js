dd7eac17de1febcf15e6b6f393b6e8123a80f834
aclaeczix <alyaalme12@yahoo.com>
2025-10-16 22:18:16 -0600

    Update users.js

--- login-server/models/users.js
+++ login-server/models/users.js
@@ -1,23 +1,6 @@
 const mongoose = require('mongoose');
 
-// Definir el esquema de un usuario
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true, // El nombre es obligatorio
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true, // El email debe ser único
-  },
-  password: {
-    type: String,
-    required: true, // La contraseña es obligatoria
-  }
-});
-
-// Crear el modelo de Usuario basado en el esquema
+const userSchema = new mongoose.Schema({});
 const Users = mongoose.model('User', userSchema);
 
 module.exports = Users;
